aaae1f2b2e746162e7cc00e48bfcffba7104f55c
Ranger <taoliang1985@gmail.com>
2025-05-21 15:24:02 +0800

    翻译

--- src/router/index.js
+++ src/router/index.js
@@ -12,47 +12,47 @@ import RouteLinksView from '../views/RouteLinksView.vue'
 export const routes = [ // Add export here
   {
     path: '/route-links',
-    name: 'RouteLinks',
+    name: '页面索引', // Translated
     component: RouteLinksView
   },
   {
     path: '/search',
-    name: 'Search',
+    name: '搜索', // Translated
     component: () => import('../views/SearchView.vue')
   },
   {
     path: '/mi',
-    name: 'mi',
+    name: '小米垂域', // Translated
     component: ChatView
   },
   {
     path: '/ocr',
-    name: 'ocr',
+    name: '搜题', // Translated
     component: OcrView
   },
   {
     path: '/movie',
-    name: 'movie',
+    name: '影视', // Translated
     component: MovieView
   },
   {
     path: '/baidu',
-    name: 'baidu',
+    name: '百度', // Translated
     component: BaiduView
   },
   {
     path: '/advanced',
-    name: 'advanced',
+    name: '高级版', // Translated
     component: AdvancedView
   },
   {
     path: '/advanced-result',
-    name: 'advanced-result',
+    name: '高级搜索结果', // Translated
     component: AdvancedResultView
   },
   {
     path: '/multi-agent',
-    name: 'multi-agent',
+    name: '多智能体', // Translated
     component: MultiAgentView
   },
   {
