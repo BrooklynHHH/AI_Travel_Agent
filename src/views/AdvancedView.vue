f98ef9d9d81623f37345775bfe86c787b5ce4b57
Ranger <taoliang1985@gmail.com>
2025-07-24 16:13:06 +0800

    修改入口名字

--- src/views/AdvancedView.vue
+++ src/views/AdvancedView.vue
@@ -188,7 +188,7 @@
               <div class="item-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 3h5v5M21 3l-7 7M8 3H3v5M3 3l7 7M16 21h5v-5M21 21l-7-7M8 21H3v-5M3 21l7-7"></path></svg>
               </div>
-              <div class="item-label">全国物流查询</div>
+              <div class="item-label">全国物流查询<br>(阿里mcp)</div>
             </div>
             <div class="category-item" style="visibility: hidden;">
               <div class="item-icon">
